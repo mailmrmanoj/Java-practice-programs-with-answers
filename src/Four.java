57587cfcd714a438bf9c17349cf1e87f11032ad5
Manoj <mailmrmanoj@gmail.com>
2018-11-08 18:53:25 +0530

    practice programs

---
+++ src/Four.java
@@ -0,0 +1,13 @@
+public class Four
+{
+    public static void main(String[] args) {
+        int i;
+        System.out.println("done");
+    }
+}
+
+/*------Output------*/
+/*
+
+done
+*/
---
+++ src/Three.java
@@ -0,0 +1,24 @@
+public class Three
+{
+    public static void main(String[] args) {
+
+        int i,j;
+        double m,n;
+        i=20;
+        j=40;
+        m=3.10;
+        n=20.30;
+        System.out.println(i + "," + j + "," + m + "," n );
+    }
+}
+/*------Output------*/
+/*
+
+Main.java:19: error: ')' expected
+		System.out.println(i + "," + j + "," + m + "," n );
+		                                              ^
+Main.java:19: error: illegal start of expression
+		System.out.println(i + "," + j + "," + m + "," n );
+		                                                 ^
+
+*/
