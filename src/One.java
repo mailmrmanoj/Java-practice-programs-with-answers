44d4392d944a7b42d4345a5581afce90a3e44186
Manoj <mailmrmanoj@gmail.com>
2018-11-08 18:40:45 +0530

    practice programs
    practice programs

---
+++ src/One.java
@@ -0,0 +1,10 @@
+public class One
+{
+    public static void main(String[] args) {
+        int i=100;
+        System.out.println(i);
+        i=200;
+        System.out.println(i);
+    }
+}
+
---
+++ src/Two.java
@@ -0,0 +1,13 @@
+public class Two
+{
+    public static void main(String[] args) {
+        int x,y,z=-10,p;
+        x=600;
+        y=p=70;
+        System.out.println(x);
+        System.out.print(y);
+        System.out.println(z);
+        System.out.println(p);
+
+    }
+}
